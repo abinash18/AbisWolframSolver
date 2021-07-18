7dc92d4d82d443f18bb3eb6981764d9604aebaa7
Abinash <abinashbhullar.as@gmail.com>
2021-07-17 18:26:59 -0600

    Images showing properly formated and updated readme

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ##A free wolfram full results and step by step api resolver.
 
-Fully frontend no suspicous proxy like the other repo [here](https://github.com/WolfreeAlpha)
+Fully frontend no suspicious proxy like the other repo [here](https://github.com/WolfreeAlpha)
 
 This program is completely rebuilt with a reformed UI and is a fully single page application.
 
-Since I removed the CORS Proxy by using jQuery's AJAX the requests to the api return faster and there is no worry of you cookies being stolen or your IP being logged. All trafic is either going from or into your computer. Execpt github pages hosting the site.
+Since I removed the CORS Proxy by using jQuery's AJAX the requests to the api return faster and there is no worry of you cookies being stolen or your IP being logged. All traffic is either going from or into your computer. Except github pages hosting the site.
 
-All javascript is perfusly documented and unobuscated so you can look through it to verify theres no funny buisness. Except the css it is minified.
+All javascript is perfusly documented and unobscured so you can look through it to verify theres no funny business. Except the css it is minified.
 
-Albiet I still give credit to the original creator for the API keys. Since one key only allows 2000 api calls a month, I am using the ones from the original repo.
+Albeit I still give credit to the original creator for the API keys. Since one key only allows 2000 api calls a month, I am using the ones from the original repo.
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /**
+ * 
  * NOTE: I am using the original creator's API Interaction script as a reference and boiler plate.
  * None of the API keys you see here are mine. CORS Proxy is not mine either.
  * All credit goes to the original creator, who unfortunately dose not have a public github 
  * account for me to link to so here is the original repository instead:
- * https://github.com/WolfreeAlpha/WolfreeAlpha.github.io
+ * https://github.com/WolfreeAlpha/WolfreeAlpha.github.io'
+ * 
+ * @author Abinash Singh
  */
 'use strict'
 
@@ -34,6 +37,21 @@ const appid = [
     '8EL8GA-7W6EVYTQ5X',
     'W4TUXQ-GA2H8KUULA',
     'UGHH75-YPX2RVU4E4',
+    '26LQEH-YT3P6T3YY9',
+    'K49A6Y-4REWHGRWW6',
+    'J77PG9-UY8A3WQ2PG',
+    'P3WLYY-2G9GA6RQGE',
+    'P7JH3K-27RHWR53JQ',
+    'L349HV-29P5JV8Y7J',
+    '77PP56-XLQK5GKUAA',
+    '59EQ3X-HE26TY2W64',
+    '8Q68TL-QA8W9GEXAA',
+    'KQRKKJ-8WHPY395HA',
+    'AAT4HU-Q3RETTGY93',
+    '7JKH84-T648HW2UV9',
+    'WYEQU3-2T55JP3WUG',
+    'T2XT8W-57PJW3L433',
+    '2557YT-52JEY65G9K',
 ];
 
 const fixedEncodeURI = string => (
@@ -50,7 +68,7 @@ const canvas = $("#dataInsertion");
 const searchBar = $("#search");
 
 function preQuery(podstate) {
-    var q = `https://api.wolframalpha.com/v2/query?${$("#search").val()}
+    var q = `https://api.wolframalpha.com/v2/query?${fixedEncodeURI($("#search").val())}
         &appid=${appid[Date.now() % appid.length]}
         &input=${location.hash = fixedEncodeURI(document.title = $("#search").val())}
         &output=json
@@ -70,9 +88,11 @@ function preQuery(podstate) {
     q = q.replaceAll(" ", '');
     //q = fixedEncodeURI(q);
     console.log(q);
-    query(q);
+    query(q, podstate);
 };
-$("#clearinput").click();
+$("#clearInput").click(function () {
+    $("#search").val("");
+});
 
 /**
  * 
@@ -82,44 +102,119 @@ $("#clearinput").click();
  */
 async function query(queryURL, usingState) {
     var _result;
-    // AJAX cause why not.
+    // AJAX cause why not. And cause it is a CORS workaround
     $.ajax({
-            type: "GET",
-            url: queryURL,
-            success: function (result) {
-                console.log(result);
-            },
-            dataType: "jsonp",
-            async: true,
-            cache: false,
-            headers: {
-                "accept": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            },
-            beforeSend: function (result) {
-                $("#loading").toggleClass("loadingHidden");
-            },
-            complete: function (result) {
-                $("#loading").toggleClass("loadingHidden");
+        type: "GET",
+        url: queryURL,
+        success: function (result) {
+            console.log(result);
+        },
+        dataType: "jsonp",
+        async: true,
+        cache: false,
+        headers: {
+            "accept": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        beforeSend: function (result) {
+            $("#loading").toggleClass("loadingHidden");
+        },
+        complete: function (result) {
+            $("#loading").toggleClass("loadingHidden");
 
-            },
-            error: function (xhr, status, error) {
-                console.log(xhr + status + error);
-            }
-        })
-        .done(function (data) {
-            showResults(data);
-        });
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr + status + error);
+        }
+    }).done(function (data) {
+        showResults(data);
+    });
 }
+
 /**
  * 
  */
 function showResults(results) {
-    var pods = null;
-    console.log(pods = results.queryresult.pods);
-    pods.forEach(pod => {
+    if (!results.queryresult.success) {
+        alert("Error" + results.queryresult.error.code + " " +
+            results.queryresult.error.msg);
+    }
+    var pods = results.queryresult.pods;
+    /* console.log(pods = results.queryresult.pods); */
+    /* pods.forEach(pod => {
         console.log(pod.title);
+    }); */
+    createSections(pods);
+}
+
+function createInfos(pod) {
+    var _infos = "";
+    var infos = pod.infos;
+    // checks if there is a infos element
+    if (infos) {
+        // checks if the infos element is an traversable array.
+        if (!Array.isArray(infos)) {
+            console.log(infos.text);
+            _infos +=
+                `<button tabindex = "0" type = "button" class = "ib">
+                <p class = "ib">${infos.text}</p>
+                </button>`
+        } else {
+            infos.forEach((info) => {
+                _infos +=
+                    `<button tabindex = "0" type = "button" class = "ib">
+                    <p class = "ib">${info.text}</p>
+                    </button>`
+            });
+        }
+    }
+
+    return _infos;
+}
+
+function createImages(pod) {
+    console.log(pod.subpods);
+    var i = "";
+    var _subpods = pod.subpods;
+
+    _subpods.forEach(im => {
+        var _i = im.img;
+        i +=
+            `<div class="c4" max-width: ${_i.width}px; max-height: ${_i.height}px;>
+                <img src="${_i.src}" alt="${_i.alt}">
+                </img>
+            </div>
+            <hr class="s"> `
     });
+
+    return i;
+}
+
+function createSections(pods) {
+    pods.forEach((pod, i) => {
+        console.log(pod.title);
+        $("#dataInsertion").html(function () {
+            var r = `
+                    <section class = "s2">
+                    <header class = "h1">
+                    <h2 class = "head1">${pod.title}:</h2>
+                    </header>
+                    <div class = "math">
+                        ${createImages(pod)}
+                    </div>
+                    <hr class = "s">
+                    <div id = "infos" class = "i1">
+                    <div id = "info1" class = "i2">
+                    <div>
+                    </div>
+                    </div>
+                    </div>
+                    </section>
+                    `
+            return $("#dataInsertion").html() + r;
+        })
+    });
+
 }
 
 $("#form").submit(async function (event) {
